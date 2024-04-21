42555260ad26639560c03b314f7a38604edc3d50
Shyam Sharki <ssk66221@gmail.com>
2024-04-21 22:24:31 +0545

    women api implement

--- src/pages/Collections/Women_Collection.tsx
+++ src/pages/Collections/Women_Collection.tsx
@@ -1,20 +1,19 @@
 import { Button } from "@/components/ui/button";
-import women1 from "/images/woman.jpeg";
-import women2 from "/images/woman2.jpeg";
-import women3 from "/images/woman3.jpeg";
-import women4 from "/images/woman4.jpeg";
-import women5 from "/images/women5.webp";
-
 import Headernavbar from "../Navbar/Headernavbar";
-
-const images = [women1, women2, women3, women4, women5];
-
+import { useGetWomenCollectionQuery } from "@/redux/features/products/productApi";
+import BounceLoader from "@/Loader/BoundeLoader";
 const Women_Collection = () => {
+  const { data: productData, isLoading } = useGetWomenCollectionQuery();
   return (
     <>
       <Headernavbar />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-6xl mx-auto mb-5 mt-32">
-        {images.map((item, index) => (
+        {isLoading && (
+          <div className="flex justify-center items-center mx-auto  px-14">
+            <BounceLoader />
+          </div>
+        )}
+        {productData?.products.map((item, index) => (
           <div
             key={index}
             className="w-full mt-8 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl"
@@ -22,11 +21,15 @@ const Women_Collection = () => {
             <img
               className="h-44 w-full object-cover rounded-xl"
               alt=""
-              src={item}
+              src={item?.image}
             />
             <div className="p-2">
-              <h2 className="font-bold text-blue-900 text-lg mb-2">Heading</h2>
-              <h6 className="font-bold text-lg mb-2 text-slate-500">Men</h6>
+              <h2 className="font-bold text-blue-900 text-lg mb-2">
+                {item?.name}
+              </h2>
+              <h6 className="font-bold text-lg mb-2 text-slate-500">
+                {item?.brand}
+              </h6>
               <p className="text-sm text-gray-600">
                 Simple Yet Beautiful Card Design with TaiwlindCss. ...
               </p>
@@ -34,7 +37,9 @@ const Women_Collection = () => {
 
             <div className="flex justify-center gap-4 mb-2">
               <Button className="bg-blue-900">Add To Card</Button>
-              <p className="text-xl font-bold mt-2 text-pink-600">Rs.250</p>
+              <p className="text-xl font-bold mt-2 text-pink-600">
+                {item?.price}
+              </p>
             </div>
           </div>
         ))}
